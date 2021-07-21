e87479c48ece928f9bd5643332acfe85d49494a8
rain <20520@126.com>
2021-07-21 20:36:58 +0800

    modify config.h

--- config.h
+++ config.h
@@ -68,10 +68,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *browsercmd[] = {"chromium", "--no-sandbox", NULL};
+static const char *upvlo[] = {"/root/scripts/volum-up.sh", NULL};
+static const char *downvlo[] = {"/root/scripts/volum-down.sh", NULL};
+static const char *mutevlo[] = {"/root/scripts/volum-toggle.sh", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+        { MODKEY,                       XK_c,      spawn,          {.v = browsercmd } },
+        { MODKEY,                       XK_bracketleft,spawn,      {.v = downvlo } },
+        { MODKEY,                       XK_backslash,spawn,        {.v = mutevlo } },
+        { MODKEY,                       XK_bracketright,spawn,     {.v = upvlo } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
