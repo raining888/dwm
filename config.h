2a2bd4aa3d0a6d12ff60564e6b0687589d8daa26
rain <20520@126.com>
2021-12-25 10:33:46 +0800

    modify config.h

--- config.h
+++ config.h
@@ -27,8 +27,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-//static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684"};
+//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684"};
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -68,7 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-static const char *browsercmd[] = {"chromium", "--no-sandbox", NULL};
+static const char *browsercmd[] = {"chromium", NULL, NULL};
 static const char *musiccmd[] = {"/root/Downloads/QQmusic-1.1.0.AppImage", "--no-sandbox", NULL};
 static const char *upvlo[] = {"/root/scripts/volum-up.sh", NULL};
 static const char *downvlo[] = {"/root/scripts/volum-down.sh", NULL};
